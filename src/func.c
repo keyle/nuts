cfd8811e3672aef094dbbd05a57a7a5241045395
keyle <keyle@capsule.org>
2024-04-18 21:26:14 +1000

    notes

--- src/func.c
+++ src/func.c
@@ -2,6 +2,12 @@
 
 // Editor functions
 
+// TODO FIXME @next from an empty file, we write backwards!
+// Saving works though. I think it's the \0 something
+// also scrolling past end of line sort of works but remembers the last line's length
+//      it's also confusing when saving because it looks like the file is longer than it is
+// TODO saving should probably end the file with a \n
+
 void func_save(void) {
     result_t res = write_file_content(ed.file_path, ed.contents.data);
     if (res.error) {
